e015057c324100c5494eeb1af47fbd38640b508a
Bhavik Patel <bhavik252patel@gmail.com>
2022-04-26 11:27:50 +0530

    modified

--- server/models/customers.js
+++ server/models/customers.js
@@ -1,11 +1,23 @@
 import mongoose from 'mongoose';
 
-const customerSchema = mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    city: {type: String, required: true},
-    company: {type: String, required: true},
-    id: {type: String}
+const customerSchema = new mongoose.Schema({
+    firstName: {
+        type: String, 
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String, 
+        required: true
+    },
+    company: {
+        type: String, 
+        required: true
+    },
 });
 
-export default mongoose.model('CustomerData', customerSchema);
+let Customer = mongoose.model('CustomerData', customerSchema);
+export default Customer;
--- server/routes/customers.js
+++ server/routes/customers.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import {getCustomerData, createCustomerData, customerCount, searchP} from "../controllers/customers.js";
+import {getCustomerData, createCustomerData, customerCount} from "../controllers/customers.js";
 
 const router = express.Router();
 
 router.get('/:customerId', getCustomerData);
 router.post('/createData', createCustomerData);
 router.get('/', customerCount);
-router.get('/:params/query', searchP);
 export default router;
